30caef1555c6aca78ea94a441b81930c81f91d63
nazimboudeffa <boudeffa@gmail.com>
2023-08-20 17:47:40 +0200

    Fixing loadings

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -7,6 +7,8 @@ import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
+import fs from "fs";
+
 export async function promptChatGPT( apiKey : string, prompt: string) {
 
   const loader = new DirectoryLoader("./src/data", {
@@ -31,18 +33,36 @@ export async function promptChatGPT( apiKey : string, prompt: string) {
 
   const model = new OpenAI({ openAIApiKey: apiKey });
 
-  const textSplitter = new RecursiveCharacterTextSplitter({
+  let vectorStore;
+
+  console.log("Checking for existing vector store...");
+  if (fs.existsSync(VECTOR_STORE_PATH)) {
+    // 14. Load the existing vector store
+    console.log("Loading existing vector store...");
+    vectorStore = await HNSWLib.load(
+      VECTOR_STORE_PATH,
+      new OpenAIEmbeddings({ openAIApiKey: apiKey })
+    );
+    console.log("Vector store loaded.");
+  } else {
+    // 15. Create a new vector store if one does not exist
+    console.log("Creating new vector store...");
+    const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
     });
-  const normalizedDocs = normalizeDocs(docs);
-  const splitDocs = await textSplitter.createDocuments(normalizedDocs)
-  
-  let vectorStore = await HNSWLib.fromDocuments(
-      splitDocs,
-      new OpenAIEmbeddings( { openAIApiKey: apiKey } )
-  );
+    const normalizedDocs = normalizeDocs(docs);
+    const splitDocs = await textSplitter.createDocuments(normalizedDocs);
 
-  await vectorStore.save(VECTOR_STORE_PATH);
+    // 16. Generate the vector store from the documents
+    vectorStore = await HNSWLib.fromDocuments(
+      splitDocs,
+      new OpenAIEmbeddings({ openAIApiKey: apiKey })
+    );
+    // 17. Save the vector store to the specified path
+    await vectorStore.save(VECTOR_STORE_PATH);
+
+    console.log("Vector store created.");
+  }
 
   const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
 
